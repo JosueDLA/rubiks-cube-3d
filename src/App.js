57e2d312376941b38e6e633c2ff51e8aef34741a
JosueDLA <josuedlavs@gmail.com>
2021-05-24 01:18:01 -0600

    Add basic 3D demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./style/App.css";
+import { Canvas } from "@react-three/fiber";
+import Box from "./components/Box";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[0, 0, 0]} />
+    </Canvas>
+  );
 }
 
 export default App;
